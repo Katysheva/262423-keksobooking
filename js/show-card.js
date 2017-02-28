d79e4fd3e89290d6ed5469acbfa731d749f278f7
Polina Katysheva <polly995@yandex.ru>
2017-02-28 10:11:55 +0300

    fixes

--- js/show-card.js
+++ js/show-card.js
@@ -1,60 +1,65 @@
 'use strict';
 
-window.showCard = function (dialog, elem, pinOffersList) {
+window.showCard = (function () {
 
-  for (var i = 0; i < pinOffersList.length; i++) {
+  return function (dialog, elem, pinOffersList) {
 
-    var pinOffer;
+    if (pinOffersList !== undefined && pinOffersList.length > 0) {
 
-    if (i === Math.floor(elem.dataset.itemIndex)) {
-      pinOffer = pinOffersList[i];
+      for (var i = 0; i < pinOffersList.length; i++) {
+
+        var pinOffer;
+
+        if (i === Math.floor(elem.dataset.itemIndex)) {
+          pinOffer = pinOffersList[i];
+        }
+      }
+
+      dialog.classList.remove('invisible');
+
+      dialog.querySelector('img[alt="Avatar"]').src = pinOffer.author.avatar;
+      dialog.querySelector('.lodge__title').innerText = pinOffer.offer.title;
+      dialog.querySelector('.lodge__address').innerText = pinOffer.offer.address;
+      dialog.querySelector('.lodge__price').innerHTML = pinOffer.offer.price + '&#x20bd;/ночь';
+      dialog.querySelector('.lodge__type').innerText = pinOffer.offer.type;
+      dialog.querySelector('.lodge__rooms-and-guests').innerHTML = pinOffer.offer.rooms + ' комнаты для ' + pinOffer.offer.guests + ' гостей';
+      dialog.querySelector('.lodge__checkin-time').innerHTML = 'Заезд после ' + pinOffer.offer.checkin + ', выезд до ' + pinOffer.offer.checkout;
+      dialog.querySelector('.lodge__description').innerHTML = pinOffer.offer.description;
+
+      // show features
+
+      var features = dialog.querySelector('.lodge__features');
+      features.innerHTML = '';
+
+      var curFeature;
+      pinOffer.offer.features.forEach(function (item) {
+
+        curFeature = document.createElement('span');
+
+        curFeature.className = 'feature__image';
+        curFeature.classList.add('feature__image--' + item);
+
+        features.appendChild(curFeature);
+      });
+
+      // show photos
+
+      var photos = dialog.querySelector('.lodge__photos');
+      photos.innerHTML = '';
+
+      pinOffer.offer.photos.forEach(function (item) {
+        showPhoto(item, photos)
+      });
     }
   }
 
-  dialog.classList.remove('invisible');
+  function showPhoto(imgSrc, photos) {
+    var templateElement = document.querySelector('#lodge-photos-template');
+    templateElement.content.querySelector('img').src = imgSrc;
 
-  dialog.querySelector('img[alt="Avatar"]').src = pinOffer.author.avatar;
-  dialog.querySelector('.lodge__title').innerText = pinOffer.offer.title;
-  dialog.querySelector('.lodge__address').innerText = pinOffer.offer.address;
-  dialog.querySelector('.lodge__price').innerHTML = pinOffer.offer.price + '&#x20bd;/ночь';
-  dialog.querySelector('.lodge__type').innerText = pinOffer.offer.type;
-  dialog.querySelector('.lodge__rooms-and-guests').innerHTML = pinOffer.offer.rooms + ' комнаты для ' + pinOffer.offer.guests + ' гостей';
-  dialog.querySelector('.lodge__checkin-time').innerHTML = 'Заезд после ' + pinOffer.offer.checkin + ', выезд до ' + pinOffer.offer.checkout;
-  dialog.querySelector('.lodge__description').innerHTML = pinOffer.offer.description;
-
-  // show features
-
-  var features = dialog.querySelector('.lodge__features');
-  features.innerHTML = '';
-
-  var curFeature;
-  pinOffer.offer.features.forEach(function (item) {
-
-    curFeature = document.createElement('span');
-
-    curFeature.className = 'feature__image';
-    curFeature.classList.add('feature__image--' + item);
-
-    features.appendChild(curFeature);
-  });
-
-  // show photos
-
-  var photos = dialog.querySelector('.lodge__photos');
-  photos.innerHTML = '';
-
-  var curPhoto;
-
-  for (var j = 0; j < pinOffer.offer.photos.length; j++) {
-    var item = pinOffer.offer.photos[j];
-
-    curPhoto = document.createElement('img');
-
-    curPhoto.src = item;
-    curPhoto.alt = 'Lodge photo';
-    curPhoto.width = '52';
-    curPhoto.height = '42';
-
-    photos.appendChild(curPhoto);
+    photos.appendChild(templateElement.content.cloneNode(true));
   }
-};
+})();
+
+
+
